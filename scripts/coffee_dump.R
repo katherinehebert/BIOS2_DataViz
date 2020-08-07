ea357e0a76eb6020ede4f456f5982d328604cd21
Katherine Hébert <katherine.ahebert@gmail.com>
2020-08-07 17:00:16 -0400

    set font stuff for ggplot + edit some comments

--- scripts/coffee_dump.R
+++ scripts/coffee_dump.R
@@ -8,15 +8,44 @@ library(dplyr)
 library(magrittr)
 
 # set ggplot theme
-theme_set(theme_classic())
+theme_set(theme_classic() +
+            theme(axis.title = element_text(size = 12, face = "bold"),
+                  plot.title = element_text(size = 13, face = "bold"),
+                  legend.title = element_text(size = 11, face = "bold"),
+                  legend.text = element_text(size = 10)))
 
 # import data
 df <- read.csv("data/coffee_code.csv")
-df$CoffeeCupsPerDay <- as.character(df$CoffeeCupsPerDay)
+
 # plot of all variable pairs (super difficult to interpret/understand)
 plot(df)
 
-# Visualizing honestly - different ways to show raw data & uncertainty
+# Dimensions - overwhelming bubble plot example -----
+
+# Plot 1: Dimensions example (bubble plot)
+
+# can comment out col and size to see how the plot changes
+ggplot(na.omit(df)) +
+  geom_jitter(aes(y = CodingHours, 
+                 x = CoffeeCupsPerDay,
+                 col = CoffeeType,
+                 shape = CoffeeSolveBugs,
+  ), size = 3
+  ) +
+  # set labels for each plot element
+  labs(title = "Does coffee help programmers code?",
+       x = "Coffee consumption (cups/day)",
+       y = "Productivity (hours/day)",
+       col = "Coffee Type",
+       shape = "CoffeeSolveBugs"
+  ) +
+  scale_color_brewer(palette = "Dark2") 
+ggsave("figures/bubbleplot_coffee_4dimensions.png", 
+       device = "png", width = 20, height = 15, units = "cm", dpi = "retina")
+
+
+# Visualizing honestly - Boxplot example ----
+# different ways to show raw data & uncertainty
 
 # basic scatter plot - not interpretable, raw information
 ggplot(df) +
@@ -38,9 +67,9 @@ df_mean <- group_by(df, CodingWithoutCoffee) %>%
 ggplot() +
   geom_violin(data = df, aes(x = CodingWithoutCoffee, y = CodingHours)) +
   geom_linerange(data = df_mean,
-                aes(x = CodingWithoutCoffee, 
-                    ymin = mean_codinghours - se_codinghours,
-                    ymax = mean_codinghours + se_codinghours)) +
+                 aes(x = CodingWithoutCoffee, 
+                     ymin = mean_codinghours - se_codinghours,
+                     ymax = mean_codinghours + se_codinghours)) +
   geom_point(data = df_mean, aes(x = CodingWithoutCoffee, y = mean_codinghours), size = 1.2) +
   labs(x = "Coding without coffee", y = "Coding time (hours/day")
 
@@ -53,7 +82,9 @@ ggplot() +
                  aes(x = CodingWithoutCoffee, 
                      ymin = mean_codinghours - se_codinghours,
                      ymax = mean_codinghours + se_codinghours)) +
-  geom_point(data = df_mean, aes(x = CodingWithoutCoffee, y = mean_codinghours), size = 1.2) +
+  geom_point(data = df_mean, 
+             aes(x = CodingWithoutCoffee, 
+                 y = mean_codinghours), size = 3) +
   labs(x = "Coding without coffee", y = "Coding time (hours/day")
 
 # standard error vs. standard deviation  (colours are to be changed)
@@ -63,7 +94,7 @@ ggplot(df_mean) +
                     ymax = mean_codinghours + 1.96*sd_codinghours), col = "dodgerblue") +
   geom_point(aes(x = CodingWithoutCoffee, y = mean_codinghours), size = 3) +
   labs(x = "Coding without coffee", y = "Coding time (hours/day")
-# comparison
+# plotting both error bars for comparison
 ggplot(df_mean) +
   geom_errorbar(aes(x = CodingWithoutCoffee, 
                     ymin = mean_codinghours - 1.96*sd_codinghours,
@@ -73,5 +104,7 @@ ggplot(df_mean) +
                     ymax = mean_codinghours + se_codinghours), col = "magenta") +
   geom_point(aes(x = CodingWithoutCoffee, y = mean_codinghours), size = 3) +
   labs(x = "Coding without coffee", y = "Coding time (hours/day")
+
+
 
 
